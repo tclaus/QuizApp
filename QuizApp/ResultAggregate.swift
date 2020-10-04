5a8aedcafa2ba6b11372d7e464248d4de9920c34
Thorsten Claus <thorstenclaus@web.de>
2020-10-04 15:07:56 +0200

    Added Results Aggregate as swift type

---
+++ QuizApp/ResultAggregate.swift
@@ -0,0 +1,26 @@
+//
+//  ResultAggregate.swift
+//  DasQuiz
+//
+//  Created by Thorsten Claus on 04.10.20.
+//  Copyright © 2020 Claus-Software. All rights reserved.
+//
+
+import Foundation
+
+/// Represents game statistics , date of game and archived points
+class ResultAggregate: NSObject, Codable {
+    
+    /// Date of game
+    @objc
+    var date: Date = Date.init()
+    
+    /// Points achived in this game
+    @objc
+    var points: Float = 0.0
+    
+    /// Percent of correct answers
+    @objc
+    var percent: Float = 0.0
+    
+}
